a49581a6585c9fa4be148d94791c250f9378f0ef
Helena Paixão <hp.helenapaixao@gmail.com>
2020-07-02 19:11:01 -0400

    add page signup and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import GlobalStyle from '../src/styles/global';
 import SignIn from '../src/pages/SignIn'
+import SignUp from '../src/pages/SignUp'
 
 function App() {
   return (
     <>
     <GlobalStyle/>
   <SignIn/>
+  <SignUp/>
 
   </>
   );
