c3ff9570e60754de93e0dc48eafc162150fdebf8
Helena Paixão <hp.helenapaixao@gmail.com>
2020-07-02 17:44:55 -0400

    add tooltip

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useRef, useCallback } from "react";
 import { Container } from "./styles";
+import {FiMail} from 'react-icons/fi'
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
 
 import Button from "../../components/Button";
+import Input from "../../components/Input";
+
+
+interface SignInFormData {
+    email: string;
+    password: string;
+}
+
 
 const SignIn: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
+
+
   return (
     <Container>
-      <h1>Hey Bitch!</h1>
-
-      <Button>Hey</Button>
+        <h1>SignIn</h1>
     </Container>
   );
 };
