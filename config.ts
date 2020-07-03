51fe7040330c295104eed334930d200a290190b5
Helena Paixão <hp.helenapaixao@gmail.com>
2020-07-03 16:14:23 -0400

    add firebase

---
+++ config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB8lYLikhD3iHnJrD-dfEAPdpA-rCIYETk",
+    authDomain: "the-movies-8b7e7.firebaseapp.com",
+    databaseURL: "https://the-movies-8b7e7.firebaseio.com",
+    projectId: "the-movies-8b7e7",
+    storageBucket: "the-movies-8b7e7.appspot.com",
+    messagingSenderId: "258108316887",
+    appId: "1:258108316887:web:3556e0ba7a705a7bbd6492",
+    measurementId: "G-2PW3F3PBFV"
+  };
