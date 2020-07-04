b09f076ae3522f6c8ee66543b19f3f2790f94740
Helena Paixão <hp.helenapaixao@gmail.com>
2020-07-04 12:28:41 -0400

    add profile page  with profiles

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,15 +1,65 @@
 import React from "react";
 
-import { ProfilesContainer } from "./styles";
+import { FiPlus } from "react-icons/fi";
 
-const Profile: React.FC = () => {
+import {
+    Container,
+    Header,
+    Content,
+    Title,
+    AllProfiles,
+    CardProfile,
+    CardAvatar,
+    CardText,
+    Button,
+    AddProfile,
+} from "./styles";
+
+const Profiles: React.FC = () => {
     return (
-        <ProfilesContainer>
-            <div>
-              
-            </div>
-        </ProfilesContainer>
+        <Container>
+            <Header></Header>
+
+            <Content>
+                <Title>Escolha um perfil</Title>
+
+                <AllProfiles>
+                    <CardProfile>
+                        <CardAvatar>
+                        <img
+                                src="https://avatars3.githubusercontent.com/u/11083288?s=460&u=195f820bdb85e57d7e08038a3f8eec821421d83d&v=4"
+                                alt="Helena Paixão"
+                            />
+                        </CardAvatar>
+                        <CardText>Helena Paixão</CardText>
+                    </CardProfile>
+
+                    <CardProfile>
+                        <CardAvatar>
+                            <img src="https://occ-0-2809-1380.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABUOVXiUYacjlBLwNjhXI7yzjuheEv4j03647J1rGAtRjLkqadP2bMyykXIzsZ04QZnHJS0md3nlMNNj0HIMbQNKYr7RE.png?r=071" />
+                        </CardAvatar>
+                        <CardText>Karina</CardText>
+                    </CardProfile>
+
+                    <CardProfile>
+                        <CardAvatar>
+                            <img src="https://occ-0-2809-1380.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABYCq-HPaBmwWzyEo8UjC3jQ7a2mKJhU4uPbQwFrauKbu9_-6GpfPccnQh3UWZvsGLQ1MwLo_4YZ-kuTiAsqpq0oEdPXS.png?r=f71" />
+                        </CardAvatar>
+                        <CardText>Convidado</CardText>
+                    </CardProfile>
+
+                    <CardProfile>
+                        <AddProfile>
+                            <FiPlus className="icon" color="grey" size={150} />
+                        </AddProfile>
+                        <CardText>Novo perfil</CardText>
+                    </CardProfile>
+                </AllProfiles>
+
+                <Button>GERENCIAR PERFIS</Button>
+            </Content>
+        </Container>
     );
 };
 
-export default Profile;
+export default Profiles;
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,25 +1,121 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
-    display: flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: #972D07;
+  padding: 25px 50px;
+  display: flex;
+  flex-direction: column;
+
+  box-shadow: 10px;
+
+  img {
+    width: 100px;
+  }
 `;
 
-export const ProfilesContainer = styled.div`
-    width: 100%;
-    max-width: 1280px;
-    margin-bottom: 100px;
-    margin: 0 auto;
-    padding: 40px 0;
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
+export const Header = styled.div``;
 
-    div {
-        margin: 40px;
-        margin-top: 50%;
+export const Content = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  margin: 40px 20px;
+  flex: 1;
+`;
+
+export const Title = styled.h1`
+  color: white;
+  font-size: 4.5em;
+  font-weight: 400;
+  margin-bottom: 30px;
+`;
+
+export const AllProfiles = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 250px;
+  width: 100%;
+`;
+
+export const CardProfile = styled.div`
+  cursor: pointer;
+
+  & + div {
+    margin-left: 40px;
+  }
+
+  &:hover {
+    > div {
+      border-color: white;
     }
 
-    h1 {
-        margin-left: -18px;
-        margin-top: 20px;
+    p {
+      color: white;
     }
+  }
+`;
+
+export const CardAvatar = styled.div`
+  height: 200px;
+  width: 200px;
+  border: 0.3em solid rgba(0, 0, 0, 0.4);
+
+  img {
+    height: 190px;
+    width: 190px;
+  }
+
+  &:hover {
+    border-color: black;
+  }
+`;
+
+export const CardText = styled.p`
+  margin-top: 20px;
+  text-align: center;
+  font-weight: 400;
+  color: #ffb20f;
+  font-size: 1.5em;
+`;
+
+export const Button = styled.button`
+  margin-top: 80px;
+  background-color: white;
+  width: 330px;
+  height: 60px;
+
+  border: 1px solid #ffb20f;
+  background-color: #972D07;
+  color:#ffb20f;
+  font-size: 1.5em;
+
+  cursor: pointer;
+
+  &:hover {
+    color: white;
+    border-color: white;
+  }
+`;
+
+export const AddProfile = styled.div`
+  height: 200px;
+  width: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    height: 190px;
+    width: 190px;
+  }
+
+  &:hover {
+    border-color: white;
+  }
 `;
