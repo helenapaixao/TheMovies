fe4ef1b23df6d057890411fe2d49aeac6664ed8b
Helena Paixão <hp.helenapaixao@gmail.com>
2020-07-04 01:07:43 -0400

    add searchinput

---
+++ src/components/SearchInput/styles.ts
@@ -0,0 +1,38 @@
+ 
+import styled from 'styled-components';
+
+
+
+export const Container = styled.div`
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #212329;
+  padding: 16px;
+  width: 100%;
+  align-items: center;
+  display: flex;
+  color: #666360;
+  margin-bottom: 30px;
+  & + div {
+    margin-top: 8px;
+  }
+  input {
+    flex: 1;
+    background: transparent;
+    border: 0;
+    color: #f4ede8;
+    &::placeholder {
+      color: #666360;
+    }
+  }
+  svg {
+    margin-right: 16px;
+    color:#FFF;
+   
+  }
+`;
+
+
+export const Icon = styled.svg`
+  margin-right: 16px;
+`;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Header from "../../components/Header";
 import PopularMovies from "../../components/PopularMovies";
-import ListMovies from '../../components/ListMovies';
-import FamilyMovies from '../../components/FamilyMovies';
-import Documentary from '../../components/DocumentaryMovies';
+import ListMovies from "../../components/ListMovies";
+import FamilyMovies from "../../components/FamilyMovies";
+import Documentary from "../../components/DocumentaryMovies";
 
 const Dashboard: React.FC = () => {
     return (
         <>
             <Header />
             <PopularMovies />
-            <ListMovies/>
-            <FamilyMovies/>
-            <Documentary/>
+            <ListMovies />
+            <FamilyMovies />
+            <Documentary />
         </>
     );
 };
