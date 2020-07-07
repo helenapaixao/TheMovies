ce741d09de9bb7ad7699d5c166bf08501bc83f3c
Helena Paixão <hp.helenapaixao@gmail.com>
2020-07-07 14:40:24 -0400

    consertando erro no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,44 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { FiPower, FiSearch } from "react-icons/fi";
 import { Container, Profile } from "./styles";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../hooks/auth";
-import api from '../../services/api2'
+import api from "../../services/api2";
 
 import Logo from "../../assets/logo.svg";
 
+interface IProfiledata {
+    id: number;
+    name: string;
+    avatar: string;
+    movieGenre: string;
+}
+
 const Header: React.FC = () => {
+    const [users, setUser] = useState<IProfiledata[]>([]);
     const { signOut } = useAuth();
+
+    useEffect(() => {
+        api.get("/profile").then((resp) => {
+            setUser(resp.data);
+        });
+    }, []);
 
     return (
         <Container>
             <header>
                 <img src={Logo} alt="TheMovie" />
-
                 <nav>
                     <div>
-                        <Profile>
-                            <img
-                                src="https://avatars3.githubusercontent.com/u/11083288?s=460&u=195f820bdb85e57d7e08038a3f8eec821421d83d&v=4"
-                                alt="Helena Paixão"
-                            />
+                        {users &&
+                            users.map((profile) => (
+                                <div>
+                                    <span>Bem-vindo {profile.name}</span>
+                                    <Link to="/profile">
+                                        <strong>{profile.name}</strong>
+                                    </Link>
 
-                            <div>
-                                <span>Bem-vindo,</span>
-                                <Link to="/profile">
-                                    <strong>Helena Paixão</strong>
-                                </Link>
-                            </div>
-                            <div>
-                                <Link to="/search/">
-                                    <FiSearch />
-                                </Link>
-                                <button type="button" onClick={signOut}>
-                                    <FiPower />
-                                </button>
-                            </div>
-                        </Profile>
+                                    <div>
+                                        <Link to="/search/">
+                                            <FiSearch />
+                                        </Link>
+                                        <button type="button" onClick={signOut}>
+                                            <FiPower />
+                                        </button>
+                                    </div>
+                                </div>
+                            ))}
                     </div>
                 </nav>
             </header>
