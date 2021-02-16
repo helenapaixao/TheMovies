24f1c46fe4c6296499cadd0038d078fccebecfb7
Helena Paixão <hp.helenapaixao@gmail.com>
2021-02-16 16:16:55 -0300

    :bug: fix: arrumando essa porra

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -131,7 +131,7 @@ const SignIn: React.FC = () => {
                     Criar Conta
                 </Link>
             </Content>
-            <Image />
+            
         </Container>
     );
 };
